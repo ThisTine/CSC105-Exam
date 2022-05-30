b0cbcbfc1b007d103de22e8cb406dfdff29630c0
I_AM_TINE <sutichok46@gmail.com>
2022-05-30 15:39:38 +0700

    task-4

--- src/pages/PhotoGalleryPage.jsx
+++ src/pages/PhotoGalleryPage.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useRef, useState } from 'react'
-import { Col, Container, Row, Button, ListGroup, ListGroupItem, ListGroupItemHeading, ListGroupItemText } from 'reactstrap'
+import { Col, Container, Row, Button, ListGroup, Input, ListGroupItem, ListGroupItemHeading, ListGroupItemText, ModalBody, Modal, ModalHeader, ModalFooter } from 'reactstrap'
 import GalleryItem from '../components/GalleryItem'
 import photos from '../data/photos'
 import commentdef from '../data/comments'
@@ -13,7 +13,7 @@ export default function PhotoGalleryPage() {
     const [likes, setlikes] = useState([])
     useEffect(() => {
         if (selected) {
-            window.scrollTo(0, ref.current.offsetTop)
+            // window.scrollTo(0, ref.current.offsetTop)
             let rat = 0
             let length = 0
             comments.forEach(item => {
@@ -37,6 +37,9 @@ export default function PhotoGalleryPage() {
     const unlike = (id) => {
         setlikes(val => val.filter(item => item !== id))
     }
+    const closemodal = ()=>{
+        setselected(null)
+    }
 
     return (
         <Container>
@@ -46,7 +49,36 @@ export default function PhotoGalleryPage() {
                 <hr />
             </div>
             <div>
-                {selected && <Row className='my-3' >
+                {selected && <Modal isOpen={selected} toggle={closemodal} size='lg' scrollable>
+                    <ModalHeader toggle={closemodal} >{selected.title}</ModalHeader>
+                    <ModalBody>
+                        <Row className='my-3' >
+                            <Col xs={12} ><img alt={selected.title} className='img-fluid  shadow' src={"/images/" + selected.url} /></Col>
+                            <Col xs={12} >
+                                <h1>{selected.title}</h1>
+                                <p>Rating: {rating} </p>
+                                <p>Likes: {comments.filter(item => (item.islike && item.pId === selected.id)).length + likes.filter(item => item === selected.id).length}</p>
+                                <div className='d-flex gap-3'>
+                                    <Button onClick={() => setisopen(true)} className="mb-3" color='primary'>Add comment</Button>
+                                    <Input type='button' value="Like" onClick={() => addlike(selected.id)}
+                                        className="mb-3 btn btn-primary" color='primary' />
+                                </div>
+                                <div style={{ }}>
+                                    <ListGroup>
+                                        {comments.filter(item => (item.self && item.pId === selected.id)).map(item => <ListGroupItem key={item.id} >
+                                            <ListGroupItemHeading>{item.username} (you)</ListGroupItemHeading>
+                                            <ListGroupItemText style={{ whiteSpace: "pre-line" }}>{item.comment + `\n`} - {item.scores} score(s)</ListGroupItemText>
+                                            <Button color="danger" onClick={() => deletecomment(item.id)} >delete</Button>
+                                        </ListGroupItem>)}
+                                        {comments.filter(item => (!item.self && item.pId === selected.id)).map(item => <ListGroupItem key={item.id}>
+                                            <ListGroupItemHeading>{item.username}</ListGroupItemHeading> <ListGroupItemText style={{ whiteSpace: "pre-line" }}>{item.comment + `\n`} - {item.scores} score(s) </ListGroupItemText> </ListGroupItem>)}
+                                    </ListGroup>
+                                </div>
+                            </Col>
+                        </Row>
+                    </ModalBody>
+                </Modal>}
+                {/* {selected && <Row className='my-3' >
                     <Col xs={12} lg={6}><img alt={selected.title} className='img-fluid  shadow' src={"/images/" + selected.url} /></Col>
                     <Col xs={12} lg={6}>
                         <h1>{selected.title}</h1>
@@ -69,7 +101,7 @@ export default function PhotoGalleryPage() {
                             </ListGroup>
                         </div>
                     </Col>
-                </Row>}
+                </Row>} */}
             </div>
             <Row className='g-3' >
                 {photos.map(item => <GalleryItem key={item.id} item={item} selected={selected} isOpen={selected?.id === item.id} setselected={setselected} />)}
